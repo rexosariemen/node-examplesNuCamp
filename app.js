37eb30778a150c9a305bbd374b0d0e838224cd7a
Rex Osariemen <rexosariemen@gmail.com>
2020-07-08 23:59:12 -0400

    node callbacks and error handling

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const rect = require('./rectangle')
+const rect = require('./rectangle');
 
 function solveRect (l, w) {
   console.log(`Solving for rectangle with dimensions ${l}, ${w}`)
 
-  if (l <= 0 || w <= 0) {
-    console.log(`Rectangle dimensions should be greater then zero.
-    The values passed in were: ${l}, and ${w}`);
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}`);
-    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-  }
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log(`ERROR: ${err.message}`);
+    } else {
+      console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+      console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+    }
+  })
+  // To confirm the order of asyn operations in this func, we console.log below:
+  console.log(`This statement is logged after the call to rect()`);
 }
 
 solveRect(2, 4);
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,15 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`))
+  } else {
+    setTimeout(() => 
+      callback(null, {
+        perimeter: () => 2 * (x + y),
+        area: () => x * y,
+      }),
+      2000
+    );
+  }
+};
 
-// module.exports can also be use above, the exports is a syntactic sugar.
+// It is common practice in Node, to always handle error first
