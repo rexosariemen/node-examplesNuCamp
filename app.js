802c81d3a11fd02e63bdc8dd7f641842c0e2d4f2
Rex Osariemen <rexosariemen@gmail.com>
2020-07-08 23:17:47 -0400

    simple node module

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-}
+const rect = require('./rectangle')
 
 function solveRect (l, w) {
   console.log(`Solving for rectangle with dimensions ${l}, ${w}`)
---
+++ rectangle.js
@@ -0,0 +1,4 @@
+exports.perimeter = (x, y) => 2 * (x + y);
+exports.area = (x, y) => x * y;
+
+// module.exports can also be use above, the exports is a syntactic sugar.
